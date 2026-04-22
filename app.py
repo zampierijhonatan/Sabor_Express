b8b60c448b4ab5bf2aef767f433d19ad62f8235e
bowie <zampierijhonatan@gmail.com>
2026-04-21 22:19:11 -0300

    Adicionando Def no código

--- app.py
+++ app.py
@@ -1,15 +1,48 @@
-print(""""
+import os
+
+def exibir_nome_programa():
+    print(""""
 ██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
 ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
-╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░""")
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+""")
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar restaurante')
-print('4. sair')
+def exibir_opcoes():
+    print('1. Cadastrar restaurantes')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção: {opcao_escolhida}')
+def finalizar_app():
+    os.system('cls')
+    print('\nFinalizando o app\n')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida == 1:
+        print ('\nCadastrar restaurantes\n')
+        
+    elif opcao_escolhida == 2:
+        print('\nListar restaurantes\n')   
+        
+    elif opcao_escolhida == 3:
+        print('\nAtivar restaurante\n')
+        
+    elif opcao_escolhida == 4:
+        finalizar_app()
+        
+    else:
+        print('Opção inválida')
+        escolher_opcao()
+    
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+    
+if __name__ == '__main__':
+    main()
